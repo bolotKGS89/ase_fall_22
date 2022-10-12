436848d934054ffb501c112ba2ab44ef3e1615d7
Tesfalemmb <74550157+Tesfalemmb@users.noreply.github.com>
2022-10-12 09:28:45 +0200

    Update and rename toggle_player.py to game.py

--- toggle_player.py
+++
@@ -1,4 +0,0 @@
-def toggle_player(self):
-        """Return a toggled player."""
-        self.current_player = next(self._players)
-        # TODO: switches self.current_player to the other player.
