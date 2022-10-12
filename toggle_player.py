42840957ef769819e0ec9f383bcccf444d7458a4
Tesfalemmb <74550157+Tesfalemmb@users.noreply.github.com>
2022-10-12 09:21:07 +0200

    Create toggle_player.py

---
+++ toggle_player.py
@@ -0,0 +1,4 @@
+def toggle_player(self):
+        """Return a toggled player."""
+        self.current_player = next(self._players)
+        # TODO: switches self.current_player to the other player.
